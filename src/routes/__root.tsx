f15591d63ede6fdecb4add34f2b09aa328a95de4
sama-004 <70210929+Sama-004@users.noreply.github.com>
2026-01-04 14:11:35 +0530

    remove: devtools chore: downgrade tanstack version to vercel example

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanstackDevtools } from "@tanstack/react-devtools";
 
 import appCss from "../styles.css?url";
 
@@ -37,17 +35,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 			</head>
 			<body>
 				{children}
-				<TanstackDevtools
-					config={{
-						position: "bottom-left",
-					}}
-					plugins={[
-						{
-							name: "Tanstack Router",
-							render: <TanStackRouterDevtoolsPanel />,
-						},
-					]}
-				/>
 				<Scripts />
 			</body>
 		</html>
